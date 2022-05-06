288bff2fd06b67edaee4082d8cf034b766eeb1f1
nsadawi <n.sadawi@gmail.com>
2022-05-06 10:24:32 +0100

    first commit - pushed fully working project

---
+++ classificatio_model/model/processing/validation.py
@@ -0,0 +1,45 @@
+from typing import List, Optional, Tuple, Union
+
+import numpy as np
+import pandas as pd
+from pydantic import BaseModel, ValidationError
+
+from model.config.core import config
+from model.processing.data_manager import pre_pipeline_preparation
+
+
+def validate_inputs(*, input_data: pd.DataFrame) -> Tuple[pd.DataFrame, Optional[dict]]:
+    """Check model inputs for unprocessable values."""
+
+    pre_processed = pre_pipeline_preparation(dataframe=input_data)
+    validated_data = pre_processed[config.model_config.features].copy()
+    errors = None
+
+    try:
+        # replace numpy nans so that pydantic can validate
+        MultipleTitanicDataInputs(
+            inputs=validated_data.replace({np.nan: None}).to_dict(orient="records")
+        )
+    except ValidationError as error:
+        errors = error.json()
+
+    return validated_data, errors
+
+
+class TitanicDataInputSchema(BaseModel):
+    pclass: Optional[int]
+    name: Optional[str]
+    sex: Optional[str]
+    age: Optional[int]
+    sibsp: Optional[int]
+    parch: Optional[int]
+    ticket: Optional[int]
+    fare: Optional[float]
+    cabin: Optional[str]
+    embarked: Optional[str]
+    boat: Optional[Union[str, int]]
+    body: Optional[int]
+
+
+class MultipleTitanicDataInputs(BaseModel):
+    inputs: List[TitanicDataInputSchema]
---
+++ classificatio_model/model/train_pipeline.py
@@ -0,0 +1,36 @@
+from sklearn.model_selection import train_test_split
+
+from model.config.core import config
+from model.pipeline import titanic_pipe
+from model.processing.data_manager import load_dataset, save_pipeline
+
+
+def run_training() -> None:
+    """
+    Train the model.
+    Training data can be found here:
+    https://www.openml.org/data/get_csv/16826755/phpMYEkMl
+    """
+
+    # read training data
+    data = load_dataset(file_name=config.app_config.raw_data_file)
+
+    # divide train and test
+    X_train, X_test, y_train, y_test = train_test_split(
+        data[config.model_config.features],  # predictors
+        data[config.model_config.target],
+        test_size=config.model_config.test_size,
+        # we are setting the random seed here
+        # for reproducibility
+        random_state=config.model_config.random_state,
+    )
+
+    # fit model
+    titanic_pipe.fit(X_train, y_train)
+
+    # persist trained model
+    save_pipeline(pipeline_to_persist=titanic_pipe)
+
+
+if __name__ == "__main__":
+    run_training()
